6e6c9bdfc16680e0c0712688bcb1a6429f7b686c
Jakob Kjær-Kammersgaard <jakob@miscellus.dk>
2019-02-28 22:37:12 +0100

    more

--- pyxec.py
+++ pyxec.py
@@ -4,8 +4,6 @@ from io import StringIO
 
 # Globals
 context = {}
-pyxec_sheet = None
-
 context_setup_code = """\
 PYXEC_VERSION = "1.0"
 
@@ -28,36 +26,6 @@ def pyxec_error(e):
     print(msg)
     sublime.status_message(msg)
     raise e
-
-
-def get_pyxec_view(self):
-    global pyxec_sheet
-
-    pyxec_view_name = "Pyxec View"
-
-    window = sublime.active_window()
-    original_view = window.active_view()
-
-    try:
-        pyxec_view = pyxec_sheet.view()
-        pyxec_view.name()
-
-    except:
-        pyxec_view = None
-        for iv in window.views():
-            if iv.name() == pyxec_view_name:
-                pyxec_view = iv
-                break
-        if pyxec_view is None:
-            pyxec_view = window.new_file()
-            pyxec_view.set_name(pyxec_view_name)
-        group, index = window.get_view_index(pyxec_view)
-        pyxec_sheet = window.sheets_in_group(group)[index]
-
-    window.focus_view(pyxec_view)
-    window.focus_view(original_view)
-    return pyxec_view
-
 
 
 class PyxecInitContextCommand(sublime_plugin.Command):
@@ -96,10 +64,33 @@ class PyxecExecuteCommand(sublime_plugin.TextCommand):
 
         joined_result_strings = "\n".join(result_strings)
 
-        pyxec_view = get_pyxec_view()
+        # Get Pyxec View
+        pyxec_view_name = "Pyxec View"
+        window = sublime.active_window()
+        original_view = window.active_view()
+
+        try:
+            pyxec_view = self.pyxec_sheet.view()
+            pyxec_view.name()
+
+        except:
+            pyxec_view = None
+            for candidate_view in window.views():
+                if candidate_view.name() == pyxec_view_name:
+                    pyxec_view = candidate_view
+                    break
+            else: # no break
+                pyxec_view = window.new_file()
+                pyxec_view.set_name(pyxec_view_name)
+
+            (group, view_index) = window.get_view_index(pyxec_view)
+            self.pyxec_sheet = window.sheets_in_group(group)[view_index]
+
+        window.focus_view(pyxec_view)
         pyxec_view.erase(edit, sublime.Region(0, pyxec_view.size()))
         pyxec_view.insert(edit, pyxec_view.size(), joined_result_strings)
-        sublime.active_window().focus_view(view)
+        window.focus_view(view)
+
         sublime.status_message("Pyxec: Execution completed.")
 
 class PyxecEvaluateReplaceCommand(sublime_plugin.TextCommand):
