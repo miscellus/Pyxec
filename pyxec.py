b0c8b2e28b94e1aa654866cdbca8a27798f642cd
Jakob Kjær-Kammersgaard <jakob@miscellus.dk>
2019-02-28 22:20:28 +0100

    version number

--- pyxec.py
+++ pyxec.py
@@ -2,7 +2,13 @@ import sublime
 import sublime_plugin
 from io import StringIO
 
+# Globals
+context = {}
+pyxec_sheet = None
+
 context_setup_code = """\
+PYXEC_VERSION = "1.0"
+
 import math
 from math import *
 from itertools import *
@@ -11,18 +17,47 @@ from pprint import *
 from collections import *
 """
 
+def init_context():
+    global context
+    context.clear()
+    exec(context_setup_code, context)
+init_context()
+
 def pyxec_error(e):
     msg = "Pyxec: " + str(e)
     print(msg)
     sublime.status_message(msg)
     raise e
 
-context = {}
-def init_context():
-    global context
-    context.clear()
-    exec(context_setup_code, context)
-init_context()
+
+def get_pyxec_view(self):
+    global pyxec_sheet
+
+    pyxec_view_name = "Pyxec View"
+
+    window = sublime.active_window()
+    original_view = window.active_view()
+
+    try:
+        pyxec_view = pyxec_sheet.view()
+        pyxec_view.name()
+
+    except:
+        pyxec_view = None
+        for iv in window.views():
+            if iv.name() == pyxec_view_name:
+                pyxec_view = iv
+                break
+        if pyxec_view is None:
+            pyxec_view = window.new_file()
+            pyxec_view.set_name(pyxec_view_name)
+        group, index = window.get_view_index(pyxec_view)
+        pyxec_sheet = window.sheets_in_group(group)[index]
+
+    window.focus_view(pyxec_view)
+    window.focus_view(original_view)
+    return pyxec_view
+
 
 
 class PyxecInitContextCommand(sublime_plugin.Command):
@@ -30,35 +65,6 @@ class PyxecInitContextCommand(sublime_plugin.Command):
         init_context()
 
 class PyxecExecuteCommand(sublime_plugin.TextCommand):
-
-    pyxec_sheet = None
-
-    def get_pyxec_view(self):
-        pyxec_view_name = "Pyxec View"
-
-        window = sublime.active_window()
-        original_view = window.active_view()
-
-        try:
-            pyxec_view = pyxec_sheet.view()
-            pyxec_view.name()
-
-        except:
-            pyxec_view = None
-            for iv in window.views():
-                if iv.name() == pyxec_view_name:
-                    pyxec_view = iv
-                    break
-            if pyxec_view is None:
-                pyxec_view = window.new_file()
-                pyxec_view.set_name(pyxec_view_name)
-            group, index = window.get_view_index(pyxec_view)
-            pyxec_sheet = window.sheets_in_group(group)[index]
-
-        window.focus_view(pyxec_view)
-        window.focus_view(original_view)
-        return pyxec_view
-
     def run(self, edit):
         global context
 
